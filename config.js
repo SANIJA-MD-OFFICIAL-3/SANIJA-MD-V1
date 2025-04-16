82b54aa60bffba6c44743f5180f55b03113c3d36
SANIJA-MD-OFFICIAL-3 <ggsanija764@gmail.com>
2025-04-16 19:48:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@
      return text === fault ? true : false;
  }
  module.exports = {
- SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+ SESSION_ID: process.env.SESSION_ID || "6Px1HVBlunYS6YQBc10f",
  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
  MODE: process.env.MODE || "private",
  AUTO_VOICE: process.env.AUTO_VOICE || "false",
